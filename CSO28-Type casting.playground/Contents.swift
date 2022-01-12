0633a3a15bf7acec377bf964a3402dcb8d51bf0c
CauaSDO <cauasilva@icloud.com>
2022-01-12 03:03:22 -0300

    Initial Commit

---
+++ CSO28-Type casting.playground/Contents.swift
@@ -0,0 +1,23 @@
+//Type Casting
+
+
+/* let animals: [Animal] = [
+    Animal(color: "Preto", gender: "F", breed: "Zurubeba"),
+    Dog(color: "Marrom", gender: "M", breed: "Cocker Spaniel"),
+    Dog(color: "Marrom", gender: "M", breed: "PitBull"),
+    Duck(color: "Branco", gender: "F", breed: "Silvestre"),
+    Bird(color: "Green", gender: "F", breed: "Avestruz", canFly: false)
+]
+
+print("============")
+for animal in animals {
+    animal.emiteSound()
+    
+    if animal is Dog {
+        (animal as! Dog).run()
+    }
+    if animal is Duck {
+        (animal as! Duck).fly()
+    }
+}
+ */
